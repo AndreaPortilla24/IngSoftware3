4ea9e02822cb3885ff6513bc9fef54fe52ea4304
Julian <oscarrondon697@gmail.com>
2026-08-22 14:59:40 -0500

    Lab1

---
+++ Laboratorio 1/src/main/java/co/edu/demoacademico/controller/EstudianteController.java
@@ -0,0 +1,35 @@
+package co.edu.demoacademico.controller;
+
+import co.edu.demoacademico.service.EstudianteService;
+import jakarta.validation.Valid;
+import co.edu.demoacademico.model.Estudiante;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/estudiantes")
+public class EstudianteController {
+
+    private final EstudianteService service;
+
+    public EstudianteController(EstudianteService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public Estudiante crear(@Valid @RequestBody Estudiante estudiante) {
+        return service.crear(estudiante);
+    }
+
+    @GetMapping
+    public List<Estudiante> listar() {
+        return service.listar();
+    }
+
+    @GetMapping
+    public Optional<Estudiante> buscar(String email) {
+        return service.buscar(email);
+    }
+}
